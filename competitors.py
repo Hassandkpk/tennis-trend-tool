1b03662a12aa74e921f1f6dcc1103396d7106504
Hassandkpk <rjhassanali555@gmail.com>
2026-03-15 19:43:45 +0500

    Create competitors.py

---
+++ competitors.py
@@ -0,0 +1,10 @@
+COMPETITOR_CHANNELS = [
+    {"name": "SMASHLY", "handle": "@SMASHLY076"},
+    {"name": "Cherie Diaries", "handle": "@ChérieDiaries542"},
+    {"name": "Courtiva", "handle": "@Courtiva"},
+    {"name": "Courtside Prospect", "handle": "@CourtsideProspect"},
+    {"name": "Tennisitics", "handle": "@Tennisitics"},
+    {"name": "Center Court Vision", "handle": "@CenterCourtVision"},
+    {"name": "Red Dust Report", "handle": "@RedDustReport"},
+    {"name": "Acelytics", "handle": "@AcelyticsYT"},
+]
