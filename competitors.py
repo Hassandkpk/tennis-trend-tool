9ab312a630c5a59fe46f55bf84ce42274d2884cd
Hassandkpk <rjhassanali555@gmail.com>
2026-03-16 01:59:22 +0500

    Update competitors.py

--- competitors.py
+++ competitors.py
@@ -1,4 +1,4 @@
-COMPETITOR_CHANNELS = [
+EALA_COMPETITOR_CHANNELS = [
     {"name": "SMASHLY", "handle": "@SMASHLY076"},
     {"name": "Cherie Diaries", "handle": "@ChérieDiaries542"},
     {"name": "Courtiva", "handle": "@Courtiva"},
@@ -7,4 +7,14 @@ COMPETITOR_CHANNELS = [
     {"name": "Center Court Vision", "handle": "@CenterCourtVision"},
     {"name": "Red Dust Report", "handle": "@RedDustReport"},
     {"name": "Acelytics", "handle": "@AcelyticsYT"},
+]
+
+ALCARAZ_COMPETITOR_CHANNELS = [
+    {"name": "Alcaraz Competitor 1", "channel_id": "UC81iiMbpbqrMKGaMl2XZPmA"},
+    {"name": "Alcaraz Competitor 2", "channel_id": "UC3BZs-Yi3jE84ArZXmSuDFA"},
+    {"name": "Alcaraz Competitor 3", "channel_id": "UCdn4eR1OKax5LuWTXOcCGNQ"},
+    {"name": "Alcaraz Competitor 4", "channel_id": "UCXVhhwuKesmI4aKOORdeWtg"},
+    {"name": "Alcaraz Competitor 5", "channel_id": "UC0ODo7bFhXaJNXYaKYOLHdg"},
+    {"name": "Alcaraz Competitor 6", "channel_id": "UCuo76cYt1_GxxZK1lXv9ewg"},
+    {"name": "Alcaraz Competitor 7", "channel_id": "UC29rI1fc48Yu7mmc2skqFwQ"},
 ]
