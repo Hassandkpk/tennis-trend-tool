c9f59ddc55e30c804ff8254a800af240a69e3fae
Hassandkpk <rjhassanali555@gmail.com>
2026-03-16 02:07:14 +0500

    Update competitors.py

--- competitors.py
+++ competitors.py
@@ -8,13 +8,3 @@ EALA_COMPETITOR_CHANNELS = [
     {"name": "Red Dust Report", "handle": "@RedDustReport"},
     {"name": "Acelytics", "handle": "@AcelyticsYT"},
 ]
-
-ALCARAZ_COMPETITOR_CHANNELS = [
-    {"name": "Alcaraz Competitor 1", "channel_id": "UC81iiMbpbqrMKGaMl2XZPmA"},
-    {"name": "Alcaraz Competitor 2", "channel_id": "UC3BZs-Yi3jE84ArZXmSuDFA"},
-    {"name": "Alcaraz Competitor 3", "channel_id": "UCdn4eR1OKax5LuWTXOcCGNQ"},
-    {"name": "Alcaraz Competitor 4", "channel_id": "UCXVhhwuKesmI4aKOORdeWtg"},
-    {"name": "Alcaraz Competitor 5", "channel_id": "UC0ODo7bFhXaJNXYaKYOLHdg"},
-    {"name": "Alcaraz Competitor 6", "channel_id": "UCuo76cYt1_GxxZK1lXv9ewg"},
-    {"name": "Alcaraz Competitor 7", "channel_id": "UC29rI1fc48Yu7mmc2skqFwQ"},
-]
